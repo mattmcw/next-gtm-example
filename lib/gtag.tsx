18d7f8da8d0cabfbb47f938cc9dd0f53ff9cb5c6
mattmcw <matt@sixteenmillimeter.com>
2021-12-06 23:51:45 -0500

    Added Google Tag Manager script for tracking data on clickthrough events

---
+++ lib/gtag.tsx
@@ -0,0 +1,17 @@
+export const GA_TRACKING_ID = process.env.GOOGLE_TAG_MANAGER_ID
+
+export const pageview = (url : string) => {
+  //@ts-ignore
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  })
+}
+
+export const event = ({ action, category, label, value } : any) => {
+  //@ts-ignore
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  })
+}

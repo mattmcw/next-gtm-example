3280d608a1389e5cb8c8299a364124cd09e80c13
mattmcw <matt@sixteenmillimeter.com>
2021-12-07 00:04:47 -0500

    Heroku ENV variables are not making their way through the build process. Using dotenv and explicit definition in the next.config.js file to ensure the secrets are loaded.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+if (process.env.NODE_ENV === 'development') {
+  require('dotenv').config()
+}
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env : {
+    GOOGLE_TAG_MANAGER_ID : process.env.GOOGLE_TAG_MANAGER_ID
+  }
 }
